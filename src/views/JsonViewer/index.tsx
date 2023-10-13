893780452302a943a617234cf2fbc13588bb41a9
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-10-13 17:01:43 -0300

    feat: create buttons paginations

--- src/views/JsonViewer/index.tsx
+++ src/views/JsonViewer/index.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./styled";
 import { useJson } from "../../contexts/JsonContext";
 import { useNavigate } from "react-router-dom";
 import { formatJson } from "../../utils";
 
 const JsonViewer: React.FC = () => {
-  const { json, fileName } = useJson();
+  const { json, fileName, nextPage, prevPage, currentPage, totalPages } =
+    useJson();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,39 +17,45 @@ const JsonViewer: React.FC = () => {
 
   const formattedJson = json ? formatJson(json) : null;
 
-  const renderFormattedJson = () => {
-    if (!formattedJson) return null;
-
-    return formattedJson.split("\n").map((line, index) => {
-      const isKey = index % 2 === 0;
-      const isString = line.includes(': "');
-      const hasSquareBrackets = line.includes("[") || line.includes("]");
-
-      return (
-        <div key={index}>
-          <S.Json
-            className={isKey ? "key" : "value"}
-            hasSquareBrackets={hasSquareBrackets}
-          >
-            {isString
-              ? line.replace(
-                  /"([^"]+)": "(.*?)"/g,
-                  '"$1": <span style="color:black">"$2"</span>'
-                )
-              : line}
-          </S.Json>
-        </div>
-      );
-    });
-  };
-
   return (
     <S.Container>
       {formattedJson && fileName && (
-        <div>
+        <>
           <S.Title>{fileName}</S.Title>
-          <S.ContainerJson>{renderFormattedJson()}</S.ContainerJson>
-        </div>
+          <S.ContainerJson>
+            {formattedJson.split("\n").map((line, index) => {
+              const isKey = index % 2 === 0;
+              const isString = line.includes(': "');
+              const hasSquareBrackets =
+                line.includes("[") || line.includes("]");
+
+              return (
+                <div key={index}>
+                  <S.Json
+                    className={isKey ? "key" : "value"}
+                    hasSquareBrackets={hasSquareBrackets}
+                  >
+                    {isString
+                      ? line.replace(/"([^"]+)": "(.*?)"/g, '"$1": "$2"')
+                      : line}
+                  </S.Json>
+                </div>
+              );
+            })}
+          </S.ContainerJson>
+          <S.ContainerButton>
+            {currentPage !== 1 && (
+              <S.ButtonPagination onClick={prevPage}>
+                Previous Page
+              </S.ButtonPagination>
+            )}
+            {totalPages > 1 && currentPage !== totalPages && (
+              <S.ButtonPagination onClick={nextPage}>
+                Next Page
+              </S.ButtonPagination>
+            )}
+          </S.ContainerButton>
+        </>
       )}
     </S.Container>
   );
