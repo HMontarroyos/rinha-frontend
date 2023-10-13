e114da906d6af0e797e464be37ad8339ce7a8ca5
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-10-13 17:00:55 -0300

    feat: add news methods and context in pagination

--- src/contexts/JsonContext.tsx
+++ src/contexts/JsonContext.tsx
@@ -4,6 +4,11 @@ interface JsonContextProps {
   json: string | null;
   fileName: string;
   setJsonData: (data: string, name: string) => void;
+  nextPage: () => void;
+  prevPage: () => void;
+  currentPage: number;
+  pageSize: number;
+  totalPages: number;
 }
 
 const JsonContext = createContext<JsonContextProps | undefined>(undefined);
@@ -15,14 +20,51 @@ interface JsonProviderProps {
 export const JsonProvider: React.FC<JsonProviderProps> = ({ children }) => {
   const [json, setJson] = useState<string | null>(null);
   const [fileName, setFileName] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(1000);
 
   const setJsonData = (data: string, name: string) => {
     setJson(data);
     setFileName(name);
+    setCurrentPage(1);
+  };
+
+  const getJsonPage = () => {
+    if (
+      json &&
+      typeof currentPage === "number" &&
+      typeof pageSize === "number"
+    ) {
+      const startIndex = (currentPage - 1) * pageSize;
+      const endIndex = Math.min(startIndex + pageSize, json.length);
+      return json.slice(startIndex, endIndex);
+    }
+    return null;
+  };
+
+  const totalPages = Math.ceil((json?.length || 0) / pageSize);
+
+  const nextPage = () => {
+    setCurrentPage((prev) => Math.min(prev + 1, totalPages));
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
   };
 
   return (
-    <JsonContext.Provider value={{ json, fileName, setJsonData }}>
+    <JsonContext.Provider
+      value={{
+        json: getJsonPage(),
+        fileName,
+        setJsonData,
+        nextPage,
+        prevPage,
+        currentPage,
+        pageSize,
+        totalPages,
+      }}
+    >
       {children}
     </JsonContext.Provider>
   );
