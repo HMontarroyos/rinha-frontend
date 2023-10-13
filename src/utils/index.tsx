ab46d409efc5bbc862b5bd46095ac41658652b60
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-10-13 10:29:31 -0300

    fix: adjust format export

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,16 +1,15 @@
-const formatJson = (jsonString: string): string => {
-  try {
-    const jsonObject = JSON.parse(jsonString);
-    const formattedJson = JSON.stringify(jsonObject, null, 2);
-
-    const withoutBraces = formattedJson.slice(1, formattedJson.length - 1);
-    const unquotedKeys = withoutBraces.replace(/"([^"]+)":/g, "$1:");
-
-    return unquotedKeys;
-  } catch (error) {
-    console.error("Error parsing JSON:", error);
-    return jsonString;
-  }
-};
-
-export default formatJson;
+export const formatJson = (jsonString: string): string => {
+    try {
+      const jsonObject = JSON.parse(jsonString);
+      const formattedJson = JSON.stringify(jsonObject, null, 2);
+  
+      const withoutBraces = formattedJson.slice(1, formattedJson.length - 1);
+      const unquotedKeys = withoutBraces.replace(/"([^"]+)":/g, '$1:');
+  
+      return unquotedKeys;
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      return jsonString; 
+    }
+  };
+  
